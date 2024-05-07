d2b4968e5f776613f19395aa80d87135f54075fa
Arthur Pereira da Silva <129605534+Arthur-Pereira-da-Silva@users.noreply.github.com>
2024-05-07 19:44:47 -0300

    Add files via upload

---
+++ Lab6_singleton/test/TrianguloExceptionTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrianguloExceptionTest {
+
+    @Test
+    public void testValidaTriangulo() {
+        assertTrue(TrianguloException.validaTriangulo(3.0, 4.0, 5.0)); 
+        assertTrue(TrianguloException.validaTriangulo(5.0, 12.0, 13.0)); 
+        assertTrue(TrianguloException.validaTriangulo(7.0, 24.0, 25.0)); 
+    }
+}
